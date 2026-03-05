93e26df88a758375562f811721acc1c88c1197aa
takumigit137 <takumirecruit137@gmail.com>
2026-03-05 16:41:19 +0900

    jwtの引数仕様を修正

--- app/api/sheets/route.ts
+++ app/api/sheets/route.ts
@@ -4,12 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
     const body = await req.json();
 
-    const auth = new google.auth.JWT(
-        process.env.GOOGLE_CLIENT_EMAIL,
-        undefined,
-        process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        ["https://www.googleapis.com/auth/spreadsheets"]
-    );
+    const auth = new google.auth.JWT({
+        email: process.env.GOOGLE_CLIENT_EMAIL,
+        key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
 
     const sheets = google.sheets({ version: "v4", auth });
 
